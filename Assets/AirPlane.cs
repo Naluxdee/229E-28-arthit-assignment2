c9487152f2eb1e5c6c5727f4af5268b05d27ec77
Naluxdee <arthit.phan@bumail.net>
2024-03-06 02:39:48 +0700

    AirPlane Code Fly

---
+++ Assets/AirPlane.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AirPlane : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float enginePowerThrust, liftBooster, drag, angularDrag;
+
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Add Thrust
+            rb.AddForce(transform.forward * enginePowerThrust);
+        }
+        //Lift
+        Vector3 lift = Vector3.Project(rb.velocity, transform.forward);
+        rb.AddForce(transform.up * lift.magnitude * liftBooster);
+
+        //Drag
+        rb.drag = rb.velocity.magnitude * drag;
+        rb.angularDrag = rb.velocity.magnitude * angularDrag;
+
+        //Control left-right
+        rb.AddTorque(Input.GetAxis("Horizontal") * transform.forward * -1);
+
+        //Up-Down
+        rb.AddTorque(Input.GetAxis("Velocity") * transform.right);
+
+    }
+}
